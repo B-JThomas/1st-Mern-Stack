9fda3ca7573d7a5373472f2fb870772ebfa71770
B-JThomas <bailey.jthomas2@gmail.com>
2024-02-26 10:53:36 +1100

    Create Route for creating books

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,38 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log(req)
     return res.status(201).send('Welcome To MERN')
 });
+
+//Route for saving a new Book
+app.post('/books', async (req,res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear) 
+        {
+            return res.status(400).send({ message: 'Please input all fields!' })
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+    } catch(err) {
+       console.log(err.message)    
+       res.status(500).send({ message: err.message }) 
+    }
+})
+
 
 mongoose
     .connect(mongoDBURL)
