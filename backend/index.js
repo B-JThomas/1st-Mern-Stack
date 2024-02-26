36ed9022ab6826016754b167766ea4a287429fcf
B-JThomas <bailey.jthomas2@gmail.com>
2024-02-26 11:55:48 +1100

    Include Updaet MEthod/Route

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     return res.status(201).send('Welcome To MERN')
 });
 
-//Route for saving a new Book
+//Route CREATING a Book
 app.post('/books', async (req,res) => {
     try {
         if(!req.body.title || !req.body.author || !req.body.publishYear) 
@@ -33,6 +33,61 @@ app.post('/books', async (req,res) => {
     }
 })
 
+//Route GETTING all Books 
+
+app.get('/books', async (req,res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch(err) {
+       console.log(err.message)    
+       res.status(500).send({ message: err.message }) 
+    }
+})
+
+//Route GETTING a Books 
+
+app.get('/books/:id', async (req,res) => {
+    try {
+
+        const { id } = req.params;
+        const book = await Book.findById(id);
+
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+        return res.status(200).json(book);
+    } catch(err) {
+       console.log(err.message)    
+       res.status(500).send({ message: err.message }) 
+    }
+})
+
+//Route for UPDATING a book
+
+app.put('/books/:id', async (req,res) => {
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear) 
+        {
+            return res.status(400).send({ message: 'Please input all fields!' })
+        }
+
+        const { id } = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+        if(!result){
+            return res.status(404).json({ message: 'Book Not Found'});
+        }
+        return res.status(204).json({ message: 'Book Updates'});
+
+    } catch(err) {
+       console.log(err.message)    
+       res.status(500).send({ message: err.message }) 
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
